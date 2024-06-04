b253023328e6e03a37f059d28fccfcb6683707ee
Brian Calma <briancalmadevacc@gmail.com>
2024-06-04 22:31:45 +0800

    feat: added fullname attr

--- app/Models/User.php
+++ app/Models/User.php
@@ -45,4 +45,9 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    public function getFullNameAttribute()
+    {
+        return $this->first_name . ' ' . $this->last_name; 
+    }
 }
