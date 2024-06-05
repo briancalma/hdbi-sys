927f2f22303072dc62a9ac38fbbdb0a2f07b9afe
Brian Calma <briancalmadevacc@gmail.com>
2024-06-06 00:50:22 +0800

    feat: initial users crud

--- app/Http/Controllers/Root/UserController.php
+++ app/Http/Controllers/Root/UserController.php
@@ -3,15 +3,18 @@
 namespace App\Http\Controllers\Root;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Root\CreateUserFormRequest;
+use App\Http\Requests\Root\UpdateUserFormRequest;
 use App\Models\User;
-use Illuminate\Http\Request;
+use Illuminate\Contracts\View\View;
+use Illuminate\Http\RedirectResponse;
 
 class UserController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(): View
     {
         $users = User::where('id', '!=', auth()->user()->id)->paginate(10);
 
@@ -21,17 +24,34 @@ class UserController extends Controller
     /**
      * Show the form for creating a new resource.
      */
-    public function create()
+    public function create(): View
     {
-        //
+        return view('root.users.create');        
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(CreateUserFormRequest $request): RedirectResponse
     {
-        //
+        try {
+            $data = $request->validated();
+
+            $user = User::create([
+                'first_name' => $data['first_name'],
+                'last_name' => $data['last_name'],
+                'email' => $data['email'],
+                'password' => bcrypt($data['password']),
+            ]);
+    
+            $user->assignRole($data['role']);
+    
+            return redirect()->route('root.users.index')->with('success', 'User created successfully.');
+        }
+        catch(\Exception $e) {
+            return redirect()->route('root.users.create')->with('error', 'Failed to create user.');
+        }
+       
     }
 
     /**
@@ -45,28 +65,47 @@ class UserController extends Controller
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(string $id)
+    public function edit(User $user): View
     {
-        //
+        return view('root.users.edit')->with(compact('user'));        
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(UpdateUserFormRequest $request, User $user): RedirectResponse
     {
-        //
+        try 
+        {
+            $data = $request->validated();
+
+            if(isset($data['password'])) {
+                $data['password'] = bcrypt($data['password']);
+            } else {
+                unset($data['password']);
+            }
+
+            $updatedUserCredentials = collect($data)->except('role')->toArray();
+
+            $user->update($updatedUserCredentials);   
+
+            $user->syncRoles([$data['role']]);
+
+            return redirect()->route('root.users.index')->with('success', 'User account was updated successfully.');
+        } catch(\Exception $e) {
+            return redirect()->route('root.users.edit', $user)->with('error', 'Failed to update user.');
+        }
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(User $user)
+    public function destroy(User $user): RedirectResponse
     {
         try {
             $user->delete();
 
-            return redirect()->route('root.users.index');
+            return redirect()->route('root.users.index')->with('warning', 'User account was deactivated successfully.');
         }
         catch(\Exception $e) {
             return redirect()->route('root.users.index')->with('error', 'Failed to delete user.');
