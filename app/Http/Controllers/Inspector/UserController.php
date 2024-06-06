d19f285ee772221fa0024666dfc3694fe3a34876
Brian Calma <briancalmadevacc@gmail.com>
2024-06-06 19:34:03 +0800

    feat: inspector create user

--- app/Http/Controllers/Inspector/UserController.php
+++ app/Http/Controllers/Inspector/UserController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Inspector;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
@@ -12,7 +13,9 @@ class UserController extends Controller
      */
     public function index()
     {
-        //
+        $users = User::role('Real Estate Agent')->paginate(10);
+
+        return view('inspector.users.index')->with(compact('users'));
     }
 
     /**
