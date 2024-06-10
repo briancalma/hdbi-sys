92af34ed42dc0a91f4d8274f7e5412beff10c6d1
Brian Calma <briancalmadevacc@gmail.com>
2024-06-10 18:58:59 +0800

    fix: remove fake seeder

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -3,10 +3,7 @@
 namespace Database\Seeders;
 
 use App\Constants\Roles;
-use Faker\Factory;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use Spatie\Permission\Contracts\Role;
 
 class UsersTableSeeder extends Seeder
 {
@@ -14,24 +11,25 @@ class UsersTableSeeder extends Seeder
      * Run the database seeds.
      */
     public function run(): void
-    {
-        $faker = Factory::create();
-
-        for ($i = 0; $i < 20; $i++) {
-            $_user = [
-                'first_name' => $faker->firstName,
-                'last_name' => $faker->lastName,
-                'email' => $faker->unique()->safeEmail,
-                'password' => bcrypt('password'),
-                'role' => Roles::REAL_ESTATE_AGENT
-            ];
-        
-            $userData = collect($_user)->except('role')->toArray();
+    {   
+        /** 
+         * Uncomment line below to generate random users
+            $faker = \Faker\Factory::create() 
+            for ($i = 0; $i < 20; $i++) {
+                $_user = [
+                    'first_name' => $faker->firstName,
+                    'last_name' => $faker->lastName,
+                    'email' => $faker->unique()->safeEmail,
+                    'password' => bcrypt('password'),
+                    'role' => Roles::REAL_ESTATE_AGENT
+                ];
             
-            $user = \App\Models\User::create($userData);
-            $user->assignRole($_user['role']);
-        }
-
+                $userData = collect($_user)->except('role')->toArray();
+                
+                $user = \App\Models\User::create($userData);
+                $user->assignRole($_user['role']);
+            } 
+        */
 
         $users = [
             [
