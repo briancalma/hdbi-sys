13094886462313b821aca511bca66d7f65aa2da8
Brian Calma <briancalmadevacc@gmail.com>
2024-06-14 20:36:23 +0800

    feat: wip step 3

--- resources/views/real-estate-agent/report-orders/report-order-form/step-3.blade.php
+++ resources/views/real-estate-agent/report-orders/report-order-form/step-3.blade.php
@@ -6,16 +6,52 @@
 <div class="rounded-sm border border-stroke bg-white shadow-default dark:border-strokedark dark:bg-boxdark">
     <div class="border-b border-stroke px-6.5 py-4 dark:border-strokedark">
         <h3 class="font-medium text-black dark:text-white">
-            Step 3: Additional Information
+            Step 3: Review and Submit
         </h3>
     </div>
 
-    <form method="POST" action="{{ route('root.configurations.store') }}" x-data="{ type: 'string' }">
-        <div class="p-6.5">
-            <div class="mb-5 flex flex-col gap-6 xl:flex-row">
-                
+    
+    <div class="p-6.5 flex flex-row">
+        <div class="flex flex-row">
+            <div>
+                <label for="">Order Notes</label>
+                <p>{{ session('order.order_notes') }}</p>
+            </div>
+            <div>
+                <label for="">Total Rooms</label>
+                <p>{{ session('order.order_total_rooms') }}</p>
+            </div>
+            <div>
+                <label for="">Order Type</label>
+                <p>{{ session('order.order_type_id') }}</p>
+            </div> 
+
+            <div>
+                <label for="">Order Dwelling Notes</label>
+                <p>{{ session('order.order_dwelling_notes') }}</p>
+            </div>
+
+            <div>
+                <label for="">Order Handling Time</label>
+                <p>{{ session('order.order_handling_time_id') }}</p>
+            </div>
+
+            <div>
+                <label for="">Preferred Contact</label>
+                <p>{{ session('order.order_preferred_contact') }}</p>
+            </div>
+
+            <div>
+                <label for="">Contact Name</label>
+                <p>{{ session('order.order_contact_name') }}</p>
+            </div>
+
+            <div>
+                <label for="">Contact Email</label>
+                <p>{{ session('order.order_contact_email') }}</p>
             </div>
         </div>
-    </form>
+    </div>
+
 </div>
 @endsection
